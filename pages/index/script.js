1f315a1b2a94394173ea39b833ca2524c2b2ae5e
JoshdRod <joshrod1089@gmail.com>
2025-11-28 23:55:08 +0000

    <feat> Added display to win modal
    Win modal now displays correct answer, and boxes showing correctness of previous responses (like in wordle)

--- pages/index/script.js
+++ pages/index/script.js
@@ -1,17 +1,30 @@
 // TODO: Eventually, put this in a separate file
-let SOLUTION = expressionToDict("1/3 x^3 + 3/2 x^2 - 2x");
+let RAW_SOLUTION = "1/3 x^3 + 3/2 x^2 - 2x";
+let SOLUTION = expressionToDict(RAW_SOLUTION);
+let answerText = document.getElementById("answerText"); // Answer that appears on win modal
+answerText.innerText = `\\(${RAW_SOLUTION}\\)`;
 
 let answerBox = document.getElementById("answerBox");
 			
+// Response boxes - store previous answers
 let response1 = document.getElementById("response1");
 let response2 = document.getElementById("response2");
 let response3 = document.getElementById("response3");
 let response4 = document.getElementById("response4");
 
+// Colour boxes - store colour of previous answers for answer modal
+let colourBox1 = document.getElementById("colourBox1");
+let colourBox2 = document.getElementById("colourBox2");
+let colourBox3 = document.getElementById("colourBox3");
+let colourBox4 = document.getElementById("colourBox4");
+
 let winModal = document.getElementById("winModal");
+var winModalClose = document.getElementsByClassName("close")[0];
 
 answerBox.addEventListener("keydown", handleAnswerInputChange);
-		
+winModalClose.addEventListener("click", handleCloseModal);
+window.addEventListener("click", handleCloseModal);
+
 let responseCount = 0;
 
 function handleAnswerInputChange() {
@@ -24,18 +37,22 @@ function handleAnswerInputChange() {
 		       case 0:
 			   response1.innerText = answerBox.value;
 			   response1.style.backgroundColor = color;
+			   colourBox1.style.backgroundColor = color;
 			   break;
 		       case 1:
 			   response2.innerText = answerBox.value;
 			   response2.style.backgroundColor = color;
+			   colourBox2.style.backgroundColor = color;
 			   break;
 		       case 2:
 			   response3.innerText = answerBox.value;
 			   response3.style.backgroundColor = color;
+			   colourBox3.style.backgroundColor = color;
 			   break;
 		       case 3:
 			   response4.innerText = answerBox.value;
 			   response4.style.backgroundColor = color;
+			   colourBox4.style.backgroundColor = color;
 			   break;
 		}
 		responseCount++;
@@ -48,6 +65,9 @@ function handleAnswerInputChange() {
 	}
 }
 		
+function handleCloseModal() {
+	winModal.style.display = "none";
+}
 // TODO: Checks if input expression is a valid maths expression in the first place
 function checkValidInputExpression() {
     return;
