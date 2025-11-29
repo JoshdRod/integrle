692e83ca0b391f155b86c9618bf52708403d4cf3
JoshdRod <joshrod1089@gmail.com>
2025-11-29 22:00:59 +0000

    <chore> Changed temporary question to something more difficult
    Bracketed solutions (e.g: (1/2 x^2 - 1/4)sin(2x) still do not work, need to fix.

--- pages/index/script.js
+++ pages/index/script.js
@@ -1,5 +1,5 @@
 // TODO: Eventually, put this in a separate file
-let RAW_SOLUTION = "1/3 x^3 + 3/2 x^2 - 2x + c";
+let RAW_SOLUTION = "1/2 x^2 sin(2x) + 1/2 x cos(2x) - 1/4 sin(2x) + c";
 let SOLUTION = expressionToDict(RAW_SOLUTION);
 let answerText = document.getElementById("answerText"); // Answer that appears on win modal
 answerText.innerText = `\\(${RAW_SOLUTION}\\)`;
